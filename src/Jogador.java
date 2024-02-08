785c458e439f7d1e00688a467b074bec84bf42a7
Marcos Vinicios <mvdias1268@gmail.com>
2024-02-08 16:51:54 -0300

    Exercício 2

--- src/Jogador.java
+++ src/Jogador.java
@@ -2,8 +2,37 @@ public class Jogador {
 
     private String nome;
     private int idade;
-    private int pontuacao;
-    private int numeroTentativa;
+    private int pontuacao = 0;
+    private int numeroTentativa = 0;
+
+    public Jogador(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public int getPontuacao(){
+        return pontuacao;
+    }
+
+    public int getNumeroTentativa(){
+        return numeroTentativa;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
 
     public void adicionarPontos(){
         pontuacao++;
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Marcos", 18);
+        System.out.println("Nome: " + jogador.getNome());
+        System.out.println("Idade: " + jogador.getIdade());
+
+    }
+}
